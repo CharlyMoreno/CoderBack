16fa9b81da81c518a3351cc94dceb7caa5fcaa28
CharlyMoreno <carlitosmoreno12@hotmail.com>
2022-09-03 17:33:07 -0300

    Metodo Save

--- Entregas/2/Contenedor.js
+++ Entregas/2/Contenedor.js
@@ -10,10 +10,21 @@ class Contenedor {
         try{
             let data = await this.getAll()
             data = JSON.parse(data)
-
-            objetoASumar.id = data.length + 1
+            
+            //Copio el arreglo de los productos
+            let dataSorted = data
+            //Los ordeno en forma ascendente segun sus id's.
+            dataSorted = dataSorted.sort((a,b) => {return b.id - a.id})
+            //Selecciono el id mas alto
+            const maxId = dataSorted[0].id
+            //Al id mas alto le sumo 1
+            objetoASumar.id = maxId + 1
+            
+            //Agrego al arreglo original el objeto nuevo.
             data.push(objetoASumar)
 
+            //Escribo el archivo. 
+            //NOTA: No hay problema con el writeFile porque estoy escribiendo el array completo y no solamente el objeto nuevo.
             await fs.promises.writeFile(this.nombreArchivo,JSON.stringify(data,null,'\t'),'utf-8')
             console.log(`Elemento guardado correctamente con el id: ${objetoASumar.id}.`)
             return objetoASumar.id;
@@ -24,8 +35,17 @@ class Contenedor {
     }
 
     //Recibe el id, lo busca en el archivo y retorna el objeto. Si no existe devuelve null.
-    getById(id){
-
+    async getById(id){
+        try{
+            let data = await this.getAll()
+            data = JSON.parse(data)
+            
+            const objetoBuscado = data.find(x => x = id)
+            return objetoBuscado
+        }
+        catch(err){
+            console.log(err)
+        }
     }
 
     //Devuelve todos los objetos.
--- Entregas/2/index.js
+++ Entregas/2/index.js
@@ -3,15 +3,18 @@ const con = require('./Contenedor.js')
 const inicio = async () => {
     cont1 = new con.Contenedor('productos.txt')
     
-    const idCreado = await cont1.save({"title":"Tomate","precio":"1500","hola":"http://localhost"})
+    // const idCreado = await cont1.save({"title":"Tomate","precio":"1500","hola":"http://localhost"})
     
-    let data = await cont1.getAll()
-    data = JSON.parse(data)
+    // let data = await cont1.getAll()
+    // data = JSON.parse(data)
     
 
-    console.log("--------------------------------")
-    console.log(data)
-    console.log("--------------------------------")
+    // console.log("--------------------------------")
+    // console.log(data)
+    // console.log("--------------------------------")
+
+    console.log(await cont1.getById(1))
+
 
 }
 
