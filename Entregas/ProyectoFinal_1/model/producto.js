fdff59fd429e0e1974e35aa4f5e88ec16833a3bf
CharlyMoreno <carlitosmoreno12@hotmail.com>
2022-10-10 20:06:46 -0300

    proyecto final v1

---
+++ Entregas/ProyectoFinal_1/model/producto.js
@@ -0,0 +1,12 @@
+class Producto {
+    constructor(timestamp,nombre,descripcion,codigo,foto,precio,stock){
+        this.timestamp = timestamp;
+        this.nombre = nombre;
+        this.codigo = codigo;
+        this.descripcion = descripcion;
+        this.foto = foto;
+        this.precio = precio;  
+        this.stock  = stock;
+    }
+
+}
---
+++ Entregas/ProyectoFinal_1/router/productos.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const { Router } = express;
+const router = Router()
+const con = require('../persistencia/persistencia.js')
+const p  = new con.Contenedor('./persistencia/productos.txt');
+
+router.use(express.json());
+
+// GET ALL
+router.get('/',async (req,res)=>{
+    try{
+        const productos = await p.getAll();
+        res.json(productos)
+        res.status(200)
+    }
+    catch(err){
+        res.status(500)
+    }
+
+})
+
+// GET BY ID
+router.get('/:id',async (req,res)=>{
+    try{
+        const producto = await p.getById(req.params.id);
+        res.json(producto)
+        res.status(200)
+    }
+    catch(err){res.status(500)}
+})
+
+//POST --> Agregar producto
+router.post('/',async (req,res)=>{
+    try{
+        await p.save(req.body)
+        res.send('Post Correcto')
+        res.status(201)    
+    }
+    catch(err){res.status(500) }
+})
+
+//PUT --> Actualizar producto
+router.put('/:id',async (req,res)=>{
+    try{
+        await p.update(req.params.id,req.body)
+        res.send('Put Correcto')
+        res.status(200)
+    }
+    catch(err){res.status(500)}
+})
+
+//DELETE --> Borrar producto
+router.delete('/:id',async (req,res)=>{
+    try{
+        await p.deleteById(req.params.id)
+        res.send('Remove Correcto')
+        res.status(200)
+    }
+    catch(err){res.status(500)}
+})
+
+module.exports = router;    
