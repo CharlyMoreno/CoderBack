415f358382b31349d427393c029e4845b81c59e8
CharlyMoreno <carlitosmoreno12@hotmail.com>
2022-10-27 19:56:05 -0300

    Entrega 8

--- Semana_8/SqlLite3/Scripts_DB/select_users.js
+++ Semana_8/SqlLite3/Scripts_DB/select_users.js
@@ -1,16 +1,13 @@
 const {options} = require('../options/SQLite3.js')
 const knex = require('knex')(options);
 
-
-function realizarSelect() {
-    knex.from('users').select('*')
-    .then(rows => {
-        return rows
-    })
-    .catch((err) => console.log(err))
-    .finally(()=>{
-        knex.destroy();
-    })
-}
-
-module.exports = realizarSelect;
+knex.from('users').select('*')
+.then((rows) => {
+    for(row of rows){
+        console.log(`${row['nombre']}`)
+    }
+})
+.catch((err) => console.log(err))
+.finally(()=>{
+    knex.destroy();
+})
